ec81a97356bc1e4c132bed4ab106f8e71ed56ecf
xabuk44 <xabuk44@gmail.com>
2019-02-10 12:27:45 +0300

    add DEf - finished

--- app/main.py
+++ app/main.py
@@ -1,21 +1,24 @@
 def nps_calculator(scores):
-    scores = [10, 7, 10, 5, 9, 3, 8, 9, 10, 7, 10, 5, 9, 3, 8, 9]
 
-detractors_count = 0
-neutrals_count = 0
-promoters_count = 0
+    detractors_count = 0
+    neutrals_count = 0
+    promoters_count = 0
 
-#для каждой оценки из scores нужно делать:
-for score in scores:
-    if score < 7:
-        # detractors_count = detractors_count + 1
-        detractors_count += 1
+    #для каждой оценки из scores нужно делать:
+    for score in scores:
+        if score < 7:
+            # detractors_count = detractors_count + 1
+            detractors_count += 1
 
-    if 7 <= score <= 8:
-        neutrals_count += 1
+        if 7 <= score <= 8:
+            neutrals_count += 1
 
-    if score > 8:
-        promoters_count += 1
+        if score > 8:
+            promoters_count += 1
 
-result = (promoters_count - detractors_count) / len(scores) * 100
-print(round(result))
+    result = round((promoters_count - detractors_count) / len(scores) * 100)
+    return result
+
+print(nps_calculator([9, 10]))  # Ctrl + D
+print(nps_calculator([8, 7]))
+print(nps_calculator([5, 1]))
